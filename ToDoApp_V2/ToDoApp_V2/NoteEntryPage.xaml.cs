a01af4759bc9deca19b93f4b1084c92804e13ac5
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-03-07 11:47:11 -0800

    Add project files.

---
+++ ToDoApp_V2/ToDoApp_V2/NoteEntryPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp_V2.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp_V2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (string.IsNullOrEmpty(note.Filename))
+            {
+                var filename = Path.Combine(App.FolderPath,
+                    $"{Path.GetRandomFileName()}.notes.txt");
+
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if(note == null)
+            {
+                return;
+            }
+
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+            await Navigation.PopAsync();
+        }
+    }
+}
